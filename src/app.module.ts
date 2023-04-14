08697dfaaeaf9a4bd7fe0e9d308592061e61c892
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-04-14 21:36:59 +0200

    Set the config of the project, add Book and Author models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +13,11 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
